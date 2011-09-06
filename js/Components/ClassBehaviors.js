ae22196df62b938adb50221b73ea550c8d340a2c
Juan Enrique Munoz Zolotoochin <juanique@gmail.com>
2011-09-06 17:45:06 -0300

    -Removed core.

--- js/Components/ClassBehaviors.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $(".hover").bind("mouseover",function(){$(this).addClass("mouseOver")});
-    $(".hover").bind("mouseout",function(){$(this).removeClass("mouseOver")});
-})
